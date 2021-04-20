dc86c02962032f7bf450cdb01fce56c124ca07ee
Ian Gray <ian@graydiant.com>
2021-04-19 21:18:05 -0400

    fix layout on shop all page

--- src/pages/shop/all.js
+++ src/pages/shop/all.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { graphql } from 'gatsby'
+import { Link, Box, Container, Divider } from 'theme-ui'
 import ProductTypeCollectionPage from '../../components/ProductTypeCollectionPage'
 import Layout from '../../components/layout'
+import ResultsHeader from '../../components/collection/ResultsHeader'
+import CollectionFilterAndSort from '../../components/collection/CollectionFilterAndSort'
 
 function ShopAllPage({ data }) {
   const { nodes, totalCount } = data.allShopifyProduct
+  const [isOpen, setOpen] = useState(false)
 
   return (
     <Layout>
-      <ProductTypeCollectionPage
-        collectionTitle="Shop All Products"
-        products={nodes}
-        totalCount={totalCount}
-      />
+      <Container>
+        <ProductTypeCollectionPage
+          collectionTitle="Shop All Products"
+          products={nodes}
+        >
+          <ResultsHeader
+            title="Shop All Products"
+            resultType="products"
+            count={totalCount}
+          >
+            <Box sx={{ textAlign: 'right' }} pt={3}>
+              <Link
+                sx={{ fontSize: 1 }}
+                variant="unset"
+                onClick={() => setOpen(prev => !prev)}
+              >
+                Filter &amp; Sort
+              </Link>
+            </Box>
+          </ResultsHeader>
+          <CollectionFilterAndSort isOpen={isOpen} />
+          <Divider />
+        </ProductTypeCollectionPage>
+      </Container>
     </Layout>
   )
 }
