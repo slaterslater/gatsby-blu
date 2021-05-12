1a4d570fda69eed4e9f06eefccec82afb9890bba
Ian Gray <ian@graydiant.com>
2021-05-11 21:12:45 -0400

    mobile treatment of instagram component

--- src/components/InstagramFeed.js
+++ src/components/InstagramFeed.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link, Container, AspectImage, Box, Grid, Text, Image } from 'theme-ui'
+import { Link, Container, AspectImage, Grid, Text } from 'theme-ui'
 import useSWR from 'swr'
 
 const fetcher = (...args) => fetch(...args).then(res => res.json())
@@ -7,7 +7,7 @@ const fetcher = (...args) => fetch(...args).then(res => res.json())
 const instagramURL =
   'https://d3ejra0xbg20rg.cloudfront.net/instagram/media?shop=blubohoo.myshopify.com&resource=default'
 
-const InstagramFeed = props => {
+const InstagramFeed = () => {
   const { data, error } = useSWR(instagramURL, fetcher)
 
   if (error) return null
@@ -23,9 +23,16 @@ const InstagramFeed = props => {
         >
           Follow Us On Instagram
         </Text>
-        <Grid sx={{ gridTemplateColumns: 'repeat(6, 1fr)' }}>
+        <Grid
+          sx={{
+            gridTemplateColumns: [
+              'repeat(3, 1fr)',
+              'repeat(4, 1fr)',
+              'repeat(6, 1fr)',
+            ],
+          }}
+        >
           {data.slice(0, 12).map(post => {
-            console.log(post.media_type)
             if (['CAROUSEL_ALBUM', 'IMAGE'].includes(post.media_type))
               return (
                 <Link href={post.permalink} target="_blank">
@@ -47,52 +54,14 @@ const InstagramFeed = props => {
                   />
                 </Link>
               )
+            return null
           })}
         </Grid>
       </Container>
     )
-
-    return null
   }
+
+  return null
 }
 
 export default InstagramFeed
-
-// import { graphql, useStaticQuery } from 'gatsby'
-// // import Image from 'gatsby-image';
-
-// import React from 'react'
-
-//   return (
-//     <Box variant="sectionWrap">
-//       <Grid
-//         sx={{
-//           justifyContent: 'center',
-//           gridTemplateColumns: ['repeat(2, 1fr)', 'repeat(5, 1fr)'],
-//           gridTemplateRows: ['repeat(5, 1fr)', 'repeat(2, 1fr)'],
-//           gap: 4,
-//         }}
-//       >
-//         {data.allInstaNode.edges.map(({ node }) => (
-//           <a
-//             key={node.id}
-//             href={`https://www.instagram.com/p/${node.id}`}
-//             target="_blank"
-//           >
-//             <Image
-//               srcSet={node.thumbnails.reduce((set, thumbnail) => {
-//                 if (!set) return `${thumbnail.src} ${thumbnail.config_width}w`
-//                 return `${set}, ${thumbnail.src} ${thumbnail.config_width}w`
-//               }, '')}
-//               src={node.thumbnails[2].src}
-//               key={node.id}
-//               alt="node.title"
-//             />
-//           </a>
-//         ))}
-//       </Grid>
-//     </Box>
-//   )
-// }
-
-// export default InstagramFeed
