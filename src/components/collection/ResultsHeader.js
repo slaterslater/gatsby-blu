7f3d957c897dea801775752ba65a914bd2bd16da
Ian Gray <ian@graydiant.com>
2021-05-14 19:21:52 -0400

    truncates and expands collection description text

--- src/components/collection/ResultsHeader.js
+++ src/components/collection/ResultsHeader.js
@@ -1,12 +1,40 @@
-import React from 'react'
-import { Box, Flex, Heading, Text } from 'theme-ui'
+import React, { useMemo, useState } from 'react'
+import { Button, Link, Box, Flex, Heading, Text } from 'theme-ui'
 import pluralize from 'pluralize'
+
+const RevealText = ({ children, chars }) => {
+  const [expanded, setExpanded] = useState(false)
+
+  const truncatedText = useMemo(() => {
+    const lastSpaceIndex = children.slice(0, chars).lastIndexOf(' ')
+    return children.slice(0, lastSpaceIndex)
+  }, [children, chars])
+
+  if (children.length < chars) return <Text>{children}</Text>
+
+  if (expanded) return <Text>{children}</Text>
+
+  return (
+    <Text>
+      {truncatedText}
+      <Link
+        pl={1}
+        role="button"
+        aria-pressed={false}
+        onClick={() => setExpanded(true)}
+        sx={{ cursor: 'pointer' }}
+      >
+        ...read more
+      </Link>
+    </Text>
+  )
+}
 
 const ResultsHeader = ({ title, description, count, resultType, children }) => (
   <Flex sx={{ justifyContent: 'space-between', alignItems: 'baseline' }}>
     <Box sx={{ flex: 1 }} pr={4}>
       <Heading as="h1">{title}</Heading>
-      {description && <Text>{description}</Text>}
+      {description && <RevealText chars={180}>{description}</RevealText>}
     </Box>
     <Box>
       <Text variant="caps">{pluralize(resultType, count, true)}</Text>
