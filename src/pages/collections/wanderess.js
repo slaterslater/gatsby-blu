a5b0788403ca12b186908c0242640298f976d699
Ian Gray <ian@graydiant.com>
2021-07-16 15:52:13 -0400

    adds custom title for wanderess collection

--- src/pages/collections/wanderess.js
+++ src/pages/collections/wanderess.js
@@ -2,22 +2,34 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import CollectionGroupsView from '../../views/CollectionGroupsView'
 
-const WanderessGroupsPage = ({ path, data }) => (
-  <CollectionGroupsView
-    pageTitle="wanderess collection"
-    pageDescription="
+const WanderessGroupsPage = ({ path, data }) => {
+  const collections = data.allShopifyCollection.nodes.map(node => {
+    if (node.handle === 'wanderess-duo') {
+      return {
+        ...node,
+        title: 'most popular sets',
+      }
+    }
+    return node
+  })
+
+  return (
+    <CollectionGroupsView
+      pageTitle="wanderess collection"
+      pageDescription="
 gather charms recounting your journey of love, growth, achievement, strength, and wisdom; each unique emblem adorns your soul's truths. pick your chain, pick your charms, tell your story
     "
-    collectionOrder={[
-      'wanderess-chains',
-      'wanderess-charms',
-      'wanderess-birthstone',
-      'wanderess-duo',
-    ]}
-    collections={data.allShopifyCollection.nodes}
-    pagePath={path}
-  />
-)
+      collectionOrder={[
+        'wanderess-chains',
+        'wanderess-charms',
+        'wanderess-birthstone',
+        'wanderess-duo',
+      ]}
+      collections={collections}
+      pagePath={path}
+    />
+  )
+}
 
 export default WanderessGroupsPage
 
