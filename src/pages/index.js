df92bb84ddab6a52de0dbc1fd5800bdc1bdf0cbe
Ian Gray <ian@graydiant.com>
2021-07-29 11:16:57 -0400

    puts new homepage on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { graphql } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Container, Text, Box } from 'theme-ui'
 import Layout from '../components/layout'
-import BelovedLinks from '../components/BelovedLinks'
 
 import SEO from '../components/seo'
-import BrandStatement from '../components/BrandStatement'
-import InstagramFeed from '../components/InstagramFeed'
-import BookConsultation from '../components/BookConsultation'
-// import ReviewsSlider from '../components/ReviewsSlider'
+import HomepageReviews from '../components/HomepageReviews'
 import SanityContent from '../components/SanityContent'
+import BrandStatement from '../components/BrandStatement'
+import Medallions from '../components/Medallions'
+import CollectionSpotlight, {
+  CollectionSpotlightCard,
+} from '../components/CollectionSpotlight'
+import { HeroOuter } from '../components/content/Hero'
+import Zodiac from '../components/Zodiac'
 
 const IndexPage = ({ data }) => {
   const {
@@ -40,11 +45,23 @@ const IndexPage = ({ data }) => {
         <script type="application/ld+json">{websiteLdJSON}</script>
       </SEO>
       <SanityContent rawContent={data.sanityPage._rawContent} />
-      <BelovedLinks />
       <BrandStatement />
-      <BookConsultation />
-      {/* <ReviewsSlider /> */}
-      <InstagramFeed />
+      <CollectionSpotlight />
+      <HomepageReviews />
+      <HeroOuter
+        heading="most gifted"
+        subheading="discover the pieces you love to gift"
+        button={{ text: 'shop gifts', path: '/collections/top-gifts' }}
+        align="left"
+      >
+        <StaticImage src="../images/homepage-jul-22/most-gifted.jpg" alt="" />
+      </HeroOuter>
+      <Container variant="full">
+        <Zodiac />
+      </Container>
+      <Container variant="full">
+        <Medallions />
+      </Container>
     </Layout>
   )
 }
@@ -53,7 +70,7 @@ export default IndexPage
 
 export const query = graphql`
   {
-    sanityPage(title: { eq: "Home" }) {
+    sanityPage(title: { eq: "Next Home" }) {
       title
       _rawContent
     }
