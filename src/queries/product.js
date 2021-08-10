6a396fe52e449db3ac1e2e33718786f0415b0333
Ian Gray <ian@graydiant.com>
2021-08-10 14:43:05 -0400

    includes sku in product query for review form

--- src/queries/product.js
+++ src/queries/product.js
@@ -48,6 +48,7 @@ export const PRODUCT_QUERY = gql`
             availableForSale
             currentlyNotInStock
             title
+            sku
             selectedOptions {
               name
               value
