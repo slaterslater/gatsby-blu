1974dffd5a9564dc62c55f2247f0bfa89bc796b7
slaterslater <slaterslater@gmail.com>
2021-11-16 13:36:53 -0500

    wip

--- src/components/Zodiac.js
+++ src/components/Zodiac.js
@@ -4,13 +4,13 @@ import React from 'react'
 import ThemeLink from './app/ThemeLink'
 
 const Zodiac = props => (
-  <Container variant="full" sx={{ bg: 'bbBackground' }}>
+  <Container variant="full" sx={{ bg: '#f3f2f0' }}>
     <Flex px={[4, 0]} py={[5, 6, 7, 8]} sx={{ height: [450, 600] }}>
-      <StaticImage src="../images/zodiac/scorpio.jpg" objectFit="contain" />
+      <StaticImage src="../images/zodiac/sagittarius.jpg" objectFit="contain" />
     </Flex>
     <Box sx={{ textAlign: 'center' }}>
       <Heading variant="h1" pb={5}>
-        Zodiac: Scorpio
+        Zodiac: Sagittarius
       </Heading>
       <Text
         as="h2"
@@ -18,13 +18,13 @@ const Zodiac = props => (
         pb={5}
         sx={{ fontSize: 1, lineHeight: '1.5em' }}
       >
-        october 23 - november 21
+        november 22 - december 21
       </Text>
       <Text as="p">
         <ThemeLink
           variant="caps"
           sx={{ fontSize: 1, textDecoration: 'underline' }}
-          to="/collections/scorpio-gift-guide-1"
+          to="/collections/sagittarius"
         >
           Shop Now
         </ThemeLink>
