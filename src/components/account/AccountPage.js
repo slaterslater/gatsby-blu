d918e98afa16f1b9f0db9baf28ac371b04cdc753
slaterslater <slaterslater@gmail.com>
2022-01-20 21:02:58 -0500

    update account page padding

--- src/components/account/AccountPage.js
+++ src/components/account/AccountPage.js
@@ -8,80 +8,74 @@ const AccountPage = ({ title, subtitle, links, currentPage, children }) => {
   const { logout } = useContext(AuthContext)
 
   return (
-    <>
-      <Container as="main" pb={[0, 8]} px={[0, 6]}>
-        {/* <Breadcrumbs py={0} currentPage={currentPage} links={links}>
+    <Container as="main" p={0} pb={[0, 8]} px={[0, 6]}>
+      {/* <Breadcrumbs py={0} currentPage={currentPage} links={links}>
           <Button mr={5} variant="inverted" onClick={() => logout()}>
             logout
           </Button>
         </Breadcrumbs> */}
+      <Box
+        pt={[4, 7]}
+        sx={{
+          display: ['flex', 'grid'],
+          flexDirection: 'column-reverse',
+          gridTemplateColumns: '180px 1fr',
+        }}
+      >
         <Box
-          pt={[4, 7]}
+          as="nav"
+          mt={[3, 0]}
+          py={[6, 0]}
           sx={{
-            display: ['flex', 'grid'],
-            flexDirection: 'column-reverse',
-            gridTemplateColumns: '180px 1fr',
+            width: ['100%', 170],
+            backgroundColor: ['bbBackground', 'white'],
           }}
         >
           <Box
-            as="nav"
-            mt={[3, 0]}
-            py={[6, 0]}
-            px={[5, 0]}
+            as="ul"
+            p={0}
+            px={[6, 0]}
             sx={{
-              width: ['100%', 170],
-              backgroundColor: ['bbBackground', 'white'],
+              listStyle: 'none',
+              columnCount: [2, 1],
             }}
           >
-            <Box
-              as="ul"
-              p={0}
-              sx={{
-                listStyle: 'none',
-                columnCount: [2, 1],
-              }}
-            >
-              <Box as="li" mb={2} mx={[5, 0]}>
-                <Link as={ThemeLink} variant="caps" to="/account/orders">
-                  Orders
-                </Link>
-              </Box>
-              <Box as="li" mb={2} mx={[5, 0]}>
-                <Link as={ThemeLink} variant="caps" to="/account/wishlist">
-                  Wishlist
-                </Link>
-              </Box>
-              <Box as="li" mb={2} mx={[5, 0]}>
-                <Link
-                  role="button"
-                  variant="caps"
-                  aria-pressed={false}
-                  sx={{
-                    cursor: 'pointer',
-                  }}
-                  onClick={() => logout()}
-                >
-                  logout
-                </Link>
-              </Box>
+            <Box as="li" mb={2} mx={[5, 0]}>
+              <Link as={ThemeLink} variant="caps" to="/account/orders">
+                Orders
+              </Link>
+            </Box>
+            <Box as="li" mb={2} mx={[5, 0]}>
+              <Link as={ThemeLink} variant="caps" to="/account/wishlist">
+                Wishlist
+              </Link>
+            </Box>
+            <Box as="li" mb={2} mx={[5, 0]}>
+              <Link
+                role="button"
+                variant="caps"
+                aria-pressed={false}
+                sx={{
+                  cursor: 'pointer',
+                }}
+                onClick={() => logout()}
+              >
+                logout
+              </Link>
             </Box>
           </Box>
-          <Box>
-            <Heading
-              as="h2"
-              variant="h2"
-              sx={{ textAlign: ['center', 'left'] }}
-            >
-              {title}
-            </Heading>
-            <Text sx={{ fontSize: 2, textAlign: ['center', 'left'] }}>
-              {subtitle}
-            </Text>
-            {children}
-          </Box>
         </Box>
-      </Container>
-    </>
+        <Box>
+          <Heading as="h2" variant="h2" sx={{ textAlign: ['center', 'left'] }}>
+            {title}
+          </Heading>
+          <Text sx={{ fontSize: 2, textAlign: ['center', 'left'] }}>
+            {subtitle}
+          </Text>
+          {children}
+        </Box>
+      </Box>
+    </Container>
   )
 }
 
