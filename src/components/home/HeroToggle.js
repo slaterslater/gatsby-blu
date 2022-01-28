7428642765ab09d309a341d869a75a419ebc7f1b
slaterslater <slaterslater@gmail.com>
2022-01-27 22:20:39 -0500

    adds bg color

--- src/components/home/HeroToggle.js
+++ src/components/home/HeroToggle.js
@@ -8,34 +8,34 @@ import useInterval from '../../lib/useInterval'
 import { HeroOuter } from '../content/Hero'
 
 const MotionBox = motion(Box)
-const heroVariants = {
-  initial: {
-    opacity: 0.2,
-  },
-  in: {
-    opacity: 1,
-    transition: {
-      opacity: {
-        duration: 1,
-      },
-    },
-  },
-  out: {
-    opacity: 0.2,
-    transition: {
-      opacity: {
-        duration: 1,
-      },
-    },
-  },
-}
+// const heroVariants = {
+//   initial: {
+//     opacity: 0.2,
+//   },
+//   in: {
+//     opacity: 1,
+//     transition: {
+//       opacity: {
+//         duration: 1.5,
+//       },
+//     },
+//   },
+//   out: {
+//     opacity: 0.2,
+//     transition: {
+//       opacity: {
+//         duration: 0.8,
+//       },
+//     },
+//   },
+// }
 
 const HeroToggle = ({ heros }) => {
   const [isPaused, setIsPaused] = useState(false)
   const [current, setCurrent] = useState(0)
   const hero = heros[current]
   const { length } = heros
-  const delay = 5000 // 5 seconds
+  const delay = 6000 // 5 seconds
 
   useInterval(
     () => {
@@ -50,10 +50,15 @@ const HeroToggle = ({ heros }) => {
       <HeroOuter data={hero} align="left">
         <MotionBox
           key={`toggle-hero-${current}`}
-          initial="initial"
-          animate="in"
-          exit="out"
-          variants={heroVariants}
+          initial={{ opacity: 0.2 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0.2 }}
+          transition={{
+            opacity: { duration: 1.5 },
+            ease: 'easeInOut',
+          }}
+          sx={{ backgroundColor: 'cream' }}
+          // variants={heroVariants}
         >
           <GatsbyImage
             image={hero.image1.asset.gatsbyImageData}
