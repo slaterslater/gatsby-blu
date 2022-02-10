5167bcf4884281f7e64a27c0af9755a9fd87aa1f
slaterslater <slaterslater@gmail.com>
2022-02-10 09:47:26 -0500

    adds more product badges

--- src/components/product/ProductBadges.js
+++ src/components/product/ProductBadges.js
@@ -1,10 +1,12 @@
-import { StaticImage } from 'gatsby-plugin-image'
 import React, { useContext } from 'react'
 import { Flex, Image } from 'theme-ui'
 import { ProductContext } from './ProductContext'
 
 const allBadges = [
   'hand-carved',
+  'antique-diamond',
+  'artist-cut',
+  'one-of-a-kind',
 ]
 
 const ProductBadges = () => {
@@ -16,9 +18,14 @@ const ProductBadges = () => {
   )
   if (!badges.length) return <></>
   return (
-    <Flex sx={{flexWrap: 'wrap'}}>
+    <Flex sx={{ flexWrap: 'wrap' }}>
       {badges.map(badge => (
-        <Image key={`${badge}-badge`} src={`/badges/${badge}.jpg`} sx={{height:55, width:55}} mx={[3,1,2]} />
+        <Image
+          key={`${badge}-badge`}
+          src={`/badges/${badge}.jpg`}
+          sx={{ height: 55, width: 55 }}
+          mx={[3, 1, 2]}
+        />
       ))}
     </Flex>
   )
