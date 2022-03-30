8d38fc82657c01a48355e69a7ae22de6dcd11077
slaterslater <slaterslater@gmail.com>
2022-03-30 11:21:54 -0400

    * adds product AvailabilityDrawer component * adds available-in-store function * adds productAvailabilityButton component * updates ProductShipping component

---
+++ src/api/available-in-store.js
@@ -0,0 +1,51 @@
+// src/api/available-in-store.js
+
+import { gql } from 'graphql-request'
+import getClient from '../lib/adminApiClient'
+
+const graphQLClient = getClient()
+
+const ProductAvailableInStore = gql`
+  query ($handle: String!) {
+    productByHandle(handle: $handle) {
+      totalInventory
+      variants(first: 10) {
+        edges {
+          node {
+            inventoryItem {
+              locationsCount
+              inventoryLevels(first: 20) {
+                edges {
+                  node {
+                    available
+                    location {
+                      name
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+export default async function (req, res) {
+  const { handle } = req.body
+
+  if (!handle) {
+    return res.status(422).json({ error: 'product handle is required' })
+  }
+
+  const data = await graphQLClient.request(ProductAvailableInStore, {
+    handle,
+  })
+
+  if (!data?.productByHandle) {
+    return res.status(400).json({ error: 'no product availability data' })
+  }
+
+  res.status(201).json(data.productByHandle)
+}
