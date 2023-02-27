403b813c39a0bed41496ef463f80ab09128f5941
slaterslater <slaterslater@gmail.com>
2023-02-27 13:26:23 -0500

    sets header

--- src/api/available-in-store.js
+++ src/api/available-in-store.js
@@ -37,7 +37,7 @@ const ProductAvailableInStore = gql`
 `
 
 export default async function (req, res) {
-  // res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
   const { handle } = req.body
 
   if (!handle) {
