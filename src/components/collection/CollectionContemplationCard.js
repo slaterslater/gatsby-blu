1791beed3cd486f9c02a24ef7eba95a9656f6ddc
slaterslater <slaterslater@gmail.com>
2023-10-26 10:42:22 -0400

    updates order of card within collection grid

--- src/components/collection/CollectionContemplationCard.js
+++ src/components/collection/CollectionContemplationCard.js
@@ -20,67 +20,22 @@ const CollectionContemplationCard = ({ card }) => {
     <Box
       sx={{
         bg: 'bbBackground',
-        order: 5,
-        // gridColumn: '1/2',
-        // marginBottom: 4,
+        order: [1000, 5],
         gridColumn: 'span 2',
         height: '100%',
-        // height: 360,
       }}
     >
       <Grid
         sx={{
           height: '100%',
           gap: 4,
-          // rowGap: 8,
-          // maxWidth: 1444,
-          // gridTemplateColumns: ['1fr', 'repeat(2, minmax(250px, 1fr))'],
-          // gridTemplateColumns: ['1fr', 'repeat(2, minmax(1fr, 1fr))'],
           gridTemplateColumns: ['1fr', '1fr minmax(250px, 1fr)'],
           textAlign: 'center',
           span: { display: 'block' },
-          // '.flexCol': {
-          //   display: 'flex',
-          //   flexDirection: 'column',
-          //   justifyContent: 'space-between',
-          // },
-          // height: '100%',
         }}
-        // px={[6, 8]}
-        // py={[7, 8]}
-        // mx="auto"
         px={5}
         py={[5, 5, 0]}
       >
-        {/* <Box
-          sx={{
-            display: ['none', 'block', 'block'],
-            // alignContent: 'center',
-            // order: 0,
-            boxShadow: '2px 2px 10px 5px lightgrey',
-            borderRadius: 15,
-            // height: '100%',
-            // height: 425,
-            // height: 'calc(25vw)',
-            height: ['calc(50vw)', 'calc(50vw)', 'calc(25vw)'],
-            // maxWidth: 250,
-            // width: 'max-content',
-            maxWidth: 250,
-            maxHeight: 425,
-            img: {
-              borderRadius: 15,
-              // height: ['calc(50vw)', 'calc(50vw)', 'calc(25vw)'],
-              // height: '100%',
-              maxHeight: 425,
-              objectFit: 'contain',
-            },
-          }}
-          // my="auto"
-          mx="auto"
-          // mx={5}
-        >
-          <GatsbyImage image={image.asset.gatsbyImageData} alt={title} />
-        </Box> */}
         <Flex
           sx={{
             justifyContent: 'center',
@@ -90,12 +45,9 @@ const CollectionContemplationCard = ({ card }) => {
         >
           <Box
             sx={{
-              // boxShadow: '2px 2px 10px 5px lightgrey',
               borderRadius: 15,
               maxWidth: 250,
               bg: 'prodBackground',
-              // maxHeight: 425,
-              // maxHeight: 'fit-content',
               img: {
                 borderRadius: 15,
                 maxHeight: 425,
@@ -117,7 +69,6 @@ const CollectionContemplationCard = ({ card }) => {
               flexDirection: 'column',
               justifyContent: 'space-between',
             }}
-            // mt={2}
             mb={4}
           >
             <SubTitle title="how to amplify this energy:" subtitle={amplify} />
